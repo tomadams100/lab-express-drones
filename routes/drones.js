5fcf03f563db234df41d92f078bbc4361726466c
Tom Adams <thomasjeadams@btinternet.com>
2021-11-12 23:11:17 +0100

    finished lab

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model')
+router.use(express.json()) // for parsing application/json
+router.use(express.urlencoded({extended: true}))
 
 // require the Drone model here
-
-router.get('/drones', (req, res, next) => {
+router.route('/drones')
+.get(async(req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
-});
+  try {
+    let allDrones = await Drone.find()
+    res.render("drones/list",allDrones)
+  } catch(err) {
+    console.log(err)
+  } 
+})
 
-router.get('/drones/create', (req, res, next) => {
+router.route('/drones/create')
+.get((req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+  res.render("drones/create-form")
+})
+.post(async(req,res)=> {
+  try {
+    const {name,propellers,maxSpeed} = req.body
+    let createDrones = await Drone.create({name,propellers,maxSpeed})
+    let allDrones = await Drone.find()
+    res.render("drones/list",allDrones)
+  } catch { 
+    res.render("drones/create-form")
+  }
+})
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async(req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  try {
+    const {id} = req.params
+    let thisDrone = await Drone.findById(id)
+    res.render("drones/update-form",{name: thisDrone.name,propellers: thisDrone.propellers,maxSpeed: thisDrone.maxSpeed,id:id})
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async(req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  try {
+    const {id} = req.params
+    const {name,propellers,maxSpeed} = req.body
+    let updateDrone = await Drone.findByIdAndUpdate(id,{name: name,propellers: propellers, maxSpeed:maxSpeed})
+    let allDrones = await Drone.find()
+    res.render("drones/list",allDrones)
+  } catch {
+    const {id} = req.params
+    res.render(`/drones/${id}/edit`)
+  }
+  
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async(req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  try {
+    const {id} = req.params
+    let deleteDrone = await Drone.findByIdAndDelete(id)
+    let allDrones = await Drone.find()
+    res.render("drones/list",allDrones)
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+ const drones = [
+     {name: "drone1", propellers: 4, maxSpeed:50},
+     {name: "drone2", propellers: 6, maxSpeed:60},
+     {name: "drone3", propellers: 8, maxSpeed:70}
+ ]
+
+ // ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+
+const Drone = require('../models/Drone.model')
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+const fillDatabase = async () => {
+    try {
+        await mongoose
+        .connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+          })
+        console.log(`Connected to Mongo!`);
+        //const deleteDrones = await Drone.deleteMany()
+        Drone.create(drones)
+    }
+    catch (err) {
+        console.error("Error connecting to mongo: ", err);
+    }
+    console.log("Disconnecting from the database...");
+    //mongoose.disconnect(); 
+}
+fillDatabase()
+
